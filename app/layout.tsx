53af4fd07982014e20cabf227d35ad6e24c5f5fc
hmd-123 <hamidtchemoko9@gmail.com>
2026-01-09 02:48:09 +0100

    Icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,13 +25,9 @@ export const metadata: Metadata = {
         url: "/icon-dark-32x32.png",
         media: "(prefers-color-scheme: dark)",
       },
-      {
-        url: "/icon.svg",
-        type: "image/svg+xml",
-      },
+
     ],
     apple: "/apple-icon.png",
-    shortcut: "/icon.svg",
   },
 }
 
